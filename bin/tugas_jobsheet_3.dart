a2918e3da23ea59ddb42d37cca09b6674105837f
arielreza <arielreza111@gmail.com>
2025-09-18 15:16:26 +0700

    dart init

---
+++ bin/tugas_jobsheet_3.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_jobsheet_3/tugas_jobsheet_3.dart' as tugas_jobsheet_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_jobsheet_3.calculate()}!');
+}
---
+++ test/tugas_jobsheet_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_jobsheet_3/tugas_jobsheet_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
